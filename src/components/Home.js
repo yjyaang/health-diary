8fbe53c7251a093928bd50238fa2c9925c5ce547
yjyaang <topbuty97@gmail.com>
2023-02-14 11:23:25 +0900

    feat: calendar 추가

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container } from '../styles/style'
+import Calendar from 'react-calendar';
+import moment from 'moment';
+import 'react-calendar/dist/Calendar.css';
+import '../styles/calendar.css';
 
 const Home = () => {
+    const [value, onChange] = useState(new Date());
+
     return (
         <div className='wrap'>
-            <Container>Home</Container>
+            <Calendar
+            calendarType='Hebrew'
+            formatDay={(locale, date) => moment(date).format("DD")}
+            />
         </div>
     )
 }
